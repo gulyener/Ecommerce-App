9967de109b6e346c967523def5654e270f5cb2ed
Gul <gulpembeyener@gmail.com>
2019-12-18 20:56:26 +0100

    add next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// must restart server whenever you make changes in next.config
+module.exports = {
+  env: {
+    MONGO_SRV:
+      'mongodb+srv://hyfgraduate:hyf12345@ecommerceapp-veeho.mongodb.net/test?retryWrites=true&w=majority',
+    JWT_SECRET: '<insert-jwt-secret>',
+    CLOUDINARY_URL: '<insert-cloudinary-url>',
+    STRIPE_SECRET_KEY: '<insert-stripe-secret-key>',
+  },
+};
