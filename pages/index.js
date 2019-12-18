058ee89f96c6b7971d7b67c0b1a68c045edd4bcb
Gul <gulpembeyener@gmail.com>
2019-12-18 17:07:12 +0100

    Add products

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
-function Home() {
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+function Home({ products }) {
+  console.log(products);
+  // useEffect(() => {
+  //   getProducts();
+  // }, []);
+
+  // const getProducts = async () => {
+  //   const url = 'http://localhost:3000/api/products';
+  //   const response = await axios.get(url);
+  //   console.log(response.data);
+  // };
+
   return <>home</>;
 }
+
+// Instead of using useEffect, we can get initial data before component mounts with next js
+Home.getInitialProps = async () => {
+  // fetch data on the server
+  const url = 'http://localhost:3000/api/products';
+  const response = await axios.get(url);
+  return { products: response.data };
+  //return response data as an object
+  // note: this object will be merged with existing props
+};
 
 export default Home;
