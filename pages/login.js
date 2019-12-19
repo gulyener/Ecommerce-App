dc0992d5ddf6d326d3df27c669f768cfb6d13e3b
Gul <gulpembeyener@gmail.com>
2019-12-19 22:09:03 +0100

    Add Login functionality

--- pages/login.js
+++ pages/login.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { Button, Form, Icon, Message, Segment } from 'semantic-ui-react';
 import Link from 'next/link';
 import catchErrors from '../utils/catchErrors';
+import baseUrl from '../utils/baseUrl';
+import axios from 'axios';
+import { handleLogin } from '../utils/auth';
 
 const INITIAL_USER = { email: '', password: '' };
 
@@ -27,9 +30,12 @@ function Login() {
     try {
       setLoading(true);
       setError('');
-      console.log(user);
+      const url = `${baseUrl}/api/login`;
+      const payload = { ...user };
+      const response = await axios.post(url, payload);
+      handleLogin(response.data);
     } catch (error) {
-      catchErrors(error);
+      catchErrors(error, setError);
     } finally {
       setLoading(false);
     }
