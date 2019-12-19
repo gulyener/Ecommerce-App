4197569df44fb8e324386f1755f3c8a611a463a3
Gul <gulpembeyener@gmail.com>
2019-12-19 20:22:29 +0100

    Create User model, sign up with jwt, store token in cookies

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     MONGO_SRV:
       'mongodb+srv://hyfgraduate:hyf12345@ecommerceapp-veeho.mongodb.net/test?retryWrites=true&w=majority',
-    JWT_SECRET: '<insert-jwt-secret>',
+    JWT_SECRET: 'gulsecrettoken',
     CLOUDINARY_URL: 'https://api.cloudinary.com/v1_1/dxsn3whop/image/upload',
     STRIPE_SECRET_KEY: '<insert-stripe-secret-key>',
   },
