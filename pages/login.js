d9fae0ac17cea1fed2f6505f0598a20138bdf0d2
Gul <gulpembeyener@gmail.com>
2019-12-19 19:39:52 +0100

    Create Login & Signup Form

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Form, Icon, Message, Segment } from 'semantic-ui-react';
+import Link from 'next/link';
+import catchErrors from '../utils/catchErrors';
+
+const INITIAL_USER = { email: '', password: '' };
+
 function Login() {
-  return <>login</>;
+  const [user, setUser] = useState(INITIAL_USER);
+  const [disabled, setDisabled] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const isUser = Object.values(user).every(elem => Boolean(elem));
+    isUser ? setDisabled(false) : setDisabled(true);
+  }, [user]);
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setUser(prevState => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    try {
+      setLoading(true);
+      setError('');
+      console.log(user);
+    } catch (error) {
+      catchErrors(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <Message
+        attached
+        icon="privacy"
+        header="Welcome Back!"
+        content="Log in with email and password"
+        color="blue"
+      />
+      <Form error={Boolean(error)} loading={loading} onSubmit={handleSubmit}>
+        <Message error header="Oops!" content={error} />
+        <Segment>
+          <Form.Input
+            fluid
+            icon="envelope"
+            iconPosition="left"
+            label="Email"
+            placeholder="Email"
+            name="email"
+            type="email"
+            value={user.email}
+            onChange={handleChange}
+          />
+          <Form.Input
+            fluid
+            icon="lock"
+            iconPosition="left"
+            label="Password"
+            placeholder="Password"
+            name="password"
+            type="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+          <Button
+            disabled={disabled || loading}
+            icon="sign in"
+            type="submit"
+            color="orange"
+            content="Login"
+          />
+        </Segment>
+      </Form>
+      <Message attached="bottom" warning>
+        <Icon name="help" />
+        New user?{' '}
+        <Link href="/signup">
+          <a>Sign up here</a>
+        </Link>{' '}
+        instead.
+      </Message>
+    </>
+  );
 }
 
 export default Login;
