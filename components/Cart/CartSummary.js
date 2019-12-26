e3ba6af67cff1fb65d3d12698e1a6751219abc04
Gul <gulpembeyener@gmail.com>
2019-12-26 15:38:55 +0100

    Add cart checkout

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Segment, Divider } from 'semantic-ui-react';
 import calculateCartTotal from '../../utils/calculateCartTotal';
+import StripeCheckout from 'react-stripe-checkout';
 
-function CartSummary({ products }) {
+function CartSummary({ products, handleCheckout, success }) {
   const [cartAmount, setCartAmount] = useState(0);
   const [stripeAmount, setStripeAmount] = useState(0);
   const [isCartEmpty, setCartEmpty] = useState(false);
@@ -19,13 +20,26 @@ function CartSummary({ products }) {
       <Divider />
       <Segment clearing size="large">
         <strong>Sub total:</strong> ${cartAmount}
-        <Button
-          icon="cart"
-          disabled={isCartEmpty}
-          color="teal"
-          floated="right"
-          content="Checkout"
-        />
+        <StripeCheckout
+          name="E-commerce App"
+          amount={stripeAmount}
+          image={products.length > 0 ? products[0].product.mediaUrl : ''}
+          currency="USD"
+          shippingAddress={true}
+          billingAddress={true}
+          zipCode={true}
+          stripeKey="pk_test_CoRcXokk0XqGmFxbJj0PLGgC00Nl9uuJMf"
+          token={handleCheckout}
+          triggerEvent="onClick"
+        >
+          <Button
+            icon="cart"
+            disabled={isCartEmpty || success}
+            color="teal"
+            floated="right"
+            content="Checkout"
+          />
+        </StripeCheckout>
       </Segment>
     </>
   );
--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ module.exports = {
       'mongodb+srv://hyfgraduate:hyf12345@ecommerceapp-veeho.mongodb.net/test?retryWrites=true&w=majority',
     JWT_SECRET: 'gulsecrettoken',
     CLOUDINARY_URL: 'https://api.cloudinary.com/v1_1/dxsn3whop/image/upload',
-    STRIPE_SECRET_KEY: '<insert-stripe-secret-key>',
+    STRIPE_SECRET_KEY: 'sk_test_VrvWTsfa4LTlvf7IDgnDUjjG00T22mZ81e',
   },
 };
