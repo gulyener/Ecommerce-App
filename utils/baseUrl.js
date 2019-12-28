d345b5ae21232d471c0a6549e76889e3eadb63a6
Gul <gulpembeyener@gmail.com>
2019-12-28 19:59:44 +0100

    Add base url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,4 +1,6 @@
 const baseUrl =
-  process.env.NODE_ENV === 'production' ? 'https://ecommerce-app.now.sh' : 'http://localhost:3000';
+  process.env.NODE_ENV === 'production'
+    ? 'https://http://ecommerce-app-2020.herokuapp.com/'
+    : 'http://localhost:3000';
 
 export default baseUrl;
