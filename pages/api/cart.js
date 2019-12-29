2ac292477b74ae87c38f35c9000a042990b7ffb4
Gul <gulpembeyener@gmail.com>
2019-12-29 13:38:08 +0100

    Change cart.js

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -28,15 +28,12 @@ async function handleGetRequest(req, res) {
   if (!('authorization' in req.headers)) {
     return res.status(401).send('No authorization token');
   }
-
   try {
     const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
-
     const cart = await Cart.findOne({ user: userId }).populate({
       path: 'products.product',
       model: 'Product',
     });
-
     res.status(200).json(cart.products);
   } catch (error) {
     console.error(error);
@@ -46,43 +43,26 @@ async function handleGetRequest(req, res) {
 
 async function handlePutRequest(req, res) {
   const { quantity, productId } = req.body;
-
   if (!('authorization' in req.headers)) {
     return res.status(401).send('No authorization token');
   }
-
   try {
     const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
-
     // Get user cart based on userId
     const cart = await Cart.findOne({ user: userId });
-
     // Check if product already exists in cart
-
     const productExists = cart.products.some(doc => ObjectId(productId).equals(doc.product));
-
     // If so, increment quantity (by number provided to request)
     if (productExists) {
       await Cart.findOneAndUpdate(
-        {
-          _id: cart._id,
-          'products.product': productId,
-        },
+        { _id: cart._id, 'products.product': productId },
         { $inc: { 'products.$.quantity': quantity } },
       );
     } else {
       // If not, add new product with given quantity
       const newProduct = { quantity, product: productId };
-      await Cart.findOneAndUpdate(
-        {
-          _id: cart._id,
-        },
-        {
-          $addToSet: { products: newProduct },
-        },
-      );
+      await Cart.findOneAndUpdate({ _id: cart._id }, { $addToSet: { products: newProduct } });
     }
-
     res.status(200).send('Cart updated');
   } catch (error) {
     console.error(error);
@@ -92,25 +72,19 @@ async function handlePutRequest(req, res) {
 
 async function handleDeleteRequest(req, res) {
   const { productId } = req.query;
-
   if (!('authorization' in req.headers)) {
     return res.status(401).send('No authorization token');
   }
-
   try {
     const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
-
     const cart = await Cart.findOneAndUpdate(
-      {
-        user: userId,
-      },
+      { user: userId },
       { $pull: { products: { product: productId } } },
       { new: true },
     ).populate({
       path: 'products.product',
       model: 'Product',
     });
-
     res.status(200).json(cart.products);
   } catch (error) {
     console.error(error);
